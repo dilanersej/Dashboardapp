5673ecb652764b1763b22125e334451c87a7ee19
Christian Haase <Christian.haase.93@gmail.com>
2015-09-11 15:05:04 +0200

    service commit

---
+++ MobileReportService/MobileReportService/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MobileReportService
+{
+    
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat=WebMessageFormat.Xml, UriTemplate="/test/{name}")]
+        string Test(string name);
+    }
+}
---
+++ MobileReportService/MobileReportService/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MobileReportService
+{
+    
+    public class Service : IService
+    {
+        public string Test(string name)
+        {
+          using (var db = new ReportEntities())
+          {
+              string str = db.Database.SqlQuery<string>("SELECT CAST(CAST(Content AS VARBINARY(MAX)) AS XML) AS DashboardXML FROM ReportServer$SRVSQL2012.dbo.Catalog WHERE Name = 'DannyTest.xml'").FirstOrDefault<string>();
+              return str;
+          }
+        }
+    }
+}
