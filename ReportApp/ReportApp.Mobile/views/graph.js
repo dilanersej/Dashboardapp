46c0a27c8f3495d4cbba1d68f3a3f6c8c74fed1a
Christian Haase <Christian.haase.93@gmail.com>
2015-09-14 10:38:42 +0200

    asd

--- ReportApp/ReportApp.Mobile/views/graph.js
+++ ReportApp/ReportApp.Mobile/views/graph.js
@@ -1,7 +1,27 @@
 ﻿ReportApp.graph = function (params) {
 
+    var baseAddress = "http://localhost:8733/Design_Time_Addresses/MobileReportService/Service/";
+
+    var test = $.ajax({
+        url: baseAddress + "test/testBoard(ch og dr)",
+        type: "GET",
+        success: function (xmlObject) {
+            var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+            xmlDoc.async = false;
+            xmlDoc.load(xmlObject);
+            x = xmlDoc.getElementsByTagName("Items");
+            for (i = 0; i < x.length; i++) {
+                console.log(x[i].childNodes[0].nodeName);
+            }
+        },
+        error: function (err) {
+            console.log("STATUSCODE: " + err.status);
+            console.log("RESPONSE: " + err.responseText);
+        }
+    })
+
     var viewModel = {
-//  Put the binding properties here
+
     };
 
     return viewModel;
