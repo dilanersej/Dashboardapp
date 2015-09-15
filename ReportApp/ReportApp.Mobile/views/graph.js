9ab6940d009b8af2c44b526f602d78605396de91
Christian Haase <Christian.haase.93@gmail.com>
2015-09-15 13:53:49 +0200

    Temporary graph.js (NOT TESTED)

--- ReportApp/ReportApp.Mobile/views/graph.js
+++ ReportApp/ReportApp.Mobile/views/graph.js
@@ -2,8 +2,6 @@
 
     var baseAddress = 'http://localhost:8733/Design_Time_Addresses/MobileReportService/Service/';
 
-    var connectionString = "";
-
     //GET DASHBOARD
     var GetDashboard = $.ajax({
         url: baseAddress + 'dashboard/testBoard(ch og dr)',
@@ -12,65 +10,82 @@
             var xmlDoc = new ActiveXObject('Microsoft.XMLDOM');
             xmlDoc.async = false;
             xmlDoc.load(xmlObject);
-            SetConnectionString(xmlDoc);
-            alert(xmlDoc.xml);
-            //GET DATA
-            var dataSource = new DevExpress.data.DataSource({
-                load: function (loadOptions) {
-                    return $.ajax({
-                        url: baseAddress + '/data',
-                        type: 'POST',
-                        data: JSON.stringify({
-                            'ConnectionString': connectionString,
-                            'Data': xmlDoc.getElementsByTagName('ResultSchema').firstChild.firstChild
-                        }),
-                        success: function (data) {
-                            alert(data);
-                            return data;
-                        },
-                        error: function (err) {
-                            alert("STATUSCODE: " + err.status + " | RESPONSE: " + err.responseText);
-                        }
-                    })
-                }
-            })
-            //x = xmlDoc.getElementsByTagName('Items');
-            //for (i = 0; i < x.length; i++) {
-            //    CheckType(x[i]);
-            //}
+            var dataSource = GetData(xmlDoc);
+            x = xmlDoc.getElementsByTagName('Items');
+            for (i = 0; i < x.length; i++) {
+                CheckType(x[i], dataSource);
+            }
         },
         error: function (err) {
-            alert("STATUSCODE: " + err.status + " | RESPONSE: " + err.responseText);
+            alert("STATUSCODE: " + err.status);
         }
     })
 
-    //SET CONNECTION STRING
-    function SetConnectionString(xml) {
-        x = xml.getElementsByTagName('Parameters')
-            
-        connectionString = JSON.Stringify({
-            'UserId': x.childNodes[5].getAttribute('Value'),
-            'Password': x.childNode[6].getAttribute('Value'),
-            'Server': x.childNode[0].getAttribute('Value'),
-            'Database': x.childNode[1].getAttribute('Value')
+    //GET DATA
+    function GetData(xmlDoc) {
+        var query = BuildQuery(xmlDoc);
+        x = xmlDoc.getElementsByTagName('Parameters')[0];
+        var data = JSON.stringify({
+            'CsDTO': {
+                'UserID': x.childNodes[5].getAttribute('Value'),
+                'Password': x.childNodes[6].getAttribute('Value'),
+                'Server': x.childNodes[0].getAttribute('Value'),
+                'Database': x.childNodes[1].getAttribute('Value')
+            },
+            'Query': query
         })
-
-        alert(connectionString);
+        return dataSource = new DevExpress.data.DataSource({
+            load: function (loadOptions) {
+                return $.ajax({
+                    url: baseAddress + '/data',
+                    type: 'POST',
+                    data: data,
+                    success: function (json) {
+                        alert(json);
+                        return json;
+                    },
+                    error: function (err) {
+                        alert("STATUSCODE: " + err.status);
+                    }
+                })
+            }
+        })
     }
 
-    
+    //BUILD QUERY
+    function BuildQuery(xmlDoc) {
+        var columList = [];
+        var tableList = [];
+        var query = "SELECT";
+        var columns = xmlDoc.getElementsByTagName('Column');
+        for (i = 0; i < columns.length; i++) {
+            columList[i] = columns[i].getAttribute('Name');
+        }
+
+        for (j = 0; j < columList.length; j++) {
+            if (j === 0) {
+                query += " " + columList[j]
+            } else {
+                query += ", " + columList[j];
+            }
+        }
+
+        query += " FROM " + xmlDoc.getElementsByTagName('Table')[0].getAttribute('Name');
+
+        return query;
+    }
 
     //CHECK TYPE
-    //function CheckType(xmlType){
-    //    //DETECT THE WIDGET TYPE AND CREATE THE WIDGET
-    //    switch(xmlType.childNodes[0].nodeName){
-    //        //CHART
-    //        case 'Chart':
-    //           CreateChart(xmlType)
-    //            break;
+    function CheckType(xmlType, dataSource) {
+        //DETECT THE WIDGET TYPE AND CREATE THE WIDGET
+        switch (xmlType.childNodes[0].nodeName) {
+            //CHART
+            case 'Chart':
+                CreateChart(xmlType, dataSource)
+                break;
     //        //PIE
     //        case 'Pie':
-    //            CreatePie(xmlType);
+    //            CreatePie(xmlType, dataSource);
     //            break;
     //        //PIVOT
     //        case 'Pivot':
@@ -100,29 +115,23 @@
     //        case 'Grid':
     //            CreateGrid(xmlType);
     //            break;
-    //    }
-    //}
+        }
+    }
 
     ////CREATE CHART
-    //function CreateChart(xmlType) {
-    //    $('#chart').dxChart({
-    //        dataSource: function () {
-    //            $.ajax({
-    //                url: baseAddress + "chart",
-    //                type: "POST",
-    //                contentType: 'text/xml'
-    //            })
-    //        },
-    //        commomSeriesSettings: {
-    //            argumentField: xmlType.getElementsByTagName('Value').getAttribute('UniqueName'),
-    //            type: type
-    //        },
-    //        name: xmlType.getAttribute('Name'),
-    //        argumentField: xmlType.firstChild.firstChild.getAttribute('DataMember'),
-    //        valueField: xmlType.firstChild.lastChild.getAttribute('DataMember')
+    function CreateChart(xmlType) {
+        $('#chart').dxChart({
+            dataSource: dataSource,
+            commomSeriesSettings: {
+                argumentField: xmlType.getElementsByTagName('Value')[0].getAttribute('UniqueName'),
+                type: 'bar'
+            },
+            name: xmlType.getAttribute('Name'),
+            argumentField: xmlType.firstChild.firstChild.getAttribute('DataMember'),
+            valueField: xmlType.firstChild.lastChild.getAttribute('DataMember')
 
-    //    });
-    //}
+        });
+    }
 
     ////CREATE PIE
     //function CreatePie(xmlType) {
