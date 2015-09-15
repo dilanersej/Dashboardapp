d09ccc396fabca26af3f47f492e1af52b1b06f4e
dilanersej <dilan30@msn.com>
2015-09-15 13:58:37 +0200

    CsBuilder

---
+++ MobileReportService/MobileReportService/ConnectionStringBuilder.cs
@@ -0,0 +1,28 @@
+﻿using MobileReportService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileReportService
+{
+    public class ConnectionStringBuilder
+    {
+
+        public string CsBuilder(DataModel model)
+        {
+            //SqlConnection myConnection = new SqlConnection();
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.UserID = model.CsDTO.UserID;
+            builder.Password = model.CsDTO.Password;
+            builder.InitialCatalog = model.CsDTO.Database;
+            builder.DataSource = model.CsDTO.Server;
+            builder.ConnectTimeout = 30;
+            return builder.ConnectionString;
+        }
+    }
+}
